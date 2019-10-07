f9d10c3e5d61b5fe7eeefec35075c5b3b22d2dda
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2019-10-07 10:50:27 +0200

    Add jar for unit 3

--- unit3/unit3-start/mqtt/StringInput.java
+++ unit3/unit3-start/mqtt/StringInput.java
@@ -1,9 +1,11 @@
 package mqtt;
 
 
+import java.util.Scanner;
+
 public class StringInput extends Thread {
 
     public static void main (String[] args) {
-        
+        Scanner input = new Scanner(System.in);
     }
 }
