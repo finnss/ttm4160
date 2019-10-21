216e6ed9e7dfd7894209f369e057747b1273b00f
Eivind Jølsgard <eivinhj@stud.ntnu.no>
2019-10-21 12:02:57 +0200

    ex4 server II

--- unit4-server/src/server/UdpServer.java
+++ unit4-server/src/server/UdpServer.java
@@ -8,6 +8,9 @@ import java.net.ServerSocket;
 import java.io.*;
 import java.net.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /*public*/ class UdpClient extends Thread {
 
     
@@ -15,32 +18,44 @@ import java.net.*;
     private DatagramSocket socket;
     private boolean running;
     private byte[] buf = new byte[256];
-    private String address = "192.168.0.196";
-    private int port = 4445;
+    private String address = "192.168.0.198";
+    private int port = 4423;
 
 
     public EchoServer() {
-        socket = new DatagramSocket(4445);
+        // socket = new DatagramSocket(port);
     }
     @Override
     public void run() {
         try {
-        socket = new DatagramSocket(4445);
+        socket = new DatagramSocket(port);
         running = true;
 
         while(running){
-            DatagramPacket packet_receive = new DatagramPacket(buf, buf.length);
             System.out.println("Thread running...");
-            socket.receive(packet_receive);
+            DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
+            socket.receive(receivedPacket);
+            System.out.println("Received package");
+            String payload = new String(
+            receivedPacket.getData(), 0, receivedPacket.getLength());
+            System.out.println("Received payload: " + payload);
+            JSONObject jsonObject = new JSONObject(payload);
+            System.out.println("jsonObject:" + jsonObject.toString());
+            long timestamp = (long) jsonObject.get("currentTimestamp");
+            System.out.println("timestamp:" + timestamp);
+            String timestamp_str = "" + timestamp;
 
+
+
+            //runtime.SchedulerData.addToQueueLast("UDP_message_received", buf);
             InetAddress ipAddress = InetAddress.getByName(address);
-            DatagramPacket packet_send = new DatagramPacket(buf, buf.length, ipAddress, port);
-            String received = new String(packet_receive.getData(), 0, packet_receive.getLength());
-            if (received.equals("end")) {
+            DatagramPacket packet_send = new DatagramPacket(timestamp_str.getBytes(), timestamp_str.getBytes().length, ipAddress, port);
+            if (receivedPacket.equals("end")) {
                 running = false;
                 continue;
             }
             socket.send(packet_send);
+            System.out.println("Message sent");
         }
         socket.close();
         //System.out.println("Thread running...");
@@ -48,6 +63,8 @@ import java.net.*;
         }
         catch (Exception e) {
             e.printStackTrace();
+            socket.close();
+            return;
         }
     }
 
--- unit4-server/src/server/tester.java
+++ unit4-server/src/server/tester.java
@@ -5,7 +5,19 @@ class tester
   public static void main(String args[]) 
 { 
     //System.out.println("Hello, World"); 
-    new UdpClient().start();
+    Thread th;
+    Runnable r; 
+
+    r = new Runnable(){
+        @Override
+        public void run(){
+            new UdpClient().start();
+        }
+    };
+    
+    th = new Thread(r);
+    th.start();
+//    th.join();
 
 } 
   
